750c4990a17aa04290756c848b5a8766e8fde080
Matusal3m <matusalemdedeus@gmail.com>
2025-05-02 16:30:35 -0300

    Initial commit

---
+++ inertia/pages/errors/not_found.tsx
@@ -0,0 +1,11 @@
+export default function NotFound() {
+  return (
+    <>
+      <div className="container">
+        <div className="title">Página não encontrada</div>
+
+        <span>This page does not exist.</span>
+      </div>
+    </>
+  )
+}
---
+++ inertia/pages/home.tsx
@@ -0,0 +1,7 @@
+import Layout from '~/layouts/layout'
+
+export default function Home() {
+  return <div></div>
+}
+
+Home.layout = (page: any) => <Layout children={page} />
