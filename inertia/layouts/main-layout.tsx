0b3fc13b51a19d5e6818217af2d62020e48b385a
Matusal3m <matusalemdedeus@gmail.com>
2025-05-06 13:51:55 -0300

    fixed overflow-x

--- inertia/layouts/main-layout.tsx
+++ inertia/layouts/main-layout.tsx
@@ -4,12 +4,9 @@ export default function MainLayout({ children }: { children: React.ReactNode }) 
   const items = [{ href: '/authors', label: 'Autores' }]
 
   return (
-    <>
-      <main className="min-h-screen dark:bg-gray-900 ml-64">{children}</main>
-      <Sidebar
-        items={items}
-        className="fixed top-0 left-0 z-40 w-64 h-screen transition-transform -translate-x-full sm:translate-x-0"
-      />
-    </>
+    <div className="flex overflow-x-hidden min-h-screen dark:bg-gray-900">
+      <Sidebar items={items} className="w-[20%] h-screen hidden sm:block fixed top-0 left-0 z-40" />
+      <main className="flex-1 ml-[20%] px-4 py-6">{children}</main>
+    </div>
   )
 }
