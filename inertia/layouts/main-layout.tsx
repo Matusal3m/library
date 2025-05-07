5cb9761509713c56911034ac428a22c4d298c7e5
Matusal3m <matusalemdedeus@gmail.com>
2025-05-07 11:38:04 -0300

    add main layout responsive width

--- inertia/layouts/main-layout.tsx
+++ inertia/layouts/main-layout.tsx
@@ -12,7 +12,7 @@ export default function MainLayout({ children }: { children: React.ReactNode }) 
   return (
     <div className="flex overflow-x-hidden min-h-screen dark:bg-gray-900">
       <Sidebar items={items} className="w-[20%] h-screen hidden sm:block fixed top-0 left-0 z-40" />
-      <main className="flex-1 ml-[20%] px-4 py-6">{children}</main>
+      <main className="flex-1 ml-[20%] px-4 py-6 w-[80%]">{children}</main>
     </div>
   )
 }
