7ef928f3987e80272acfb654c85f685340988812
Matusal3m <matusalemdedeus@gmail.com>
2025-05-30 16:57:01 -0300

    add app name

--- inertia/app/app.tsx
+++ inertia/app/app.tsx
@@ -6,12 +6,12 @@ import { hydrateRoot } from 'react-dom/client'
 import { createInertiaApp } from '@inertiajs/react'
 import inertiaResolve from './inertia-resolve'
 
-const appName = import.meta.env.VITE_APP_NAME || 'AdonisJS'
+const appName = import.meta.env.VITE_APP_NAME || 'Biblioteca PP'
 
 createInertiaApp({
     progress: { color: '#5468FF' },
 
-    title: (title) => `${title} - ${appName}`,
+    title: () => appName,
 
     resolve: inertiaResolve,
 
